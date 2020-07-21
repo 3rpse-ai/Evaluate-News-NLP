218c6ead8eec8a410ac48c3eac177fd7cc6203dc
Felix Weickert <felix.weickert@dws.co.at>
2020-07-21 08:16:29 +0200

    feat: fix build bug

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,12 @@
 var path = require('path')
 const express = require('express')
-const dotenv = require('dotenv');
-dotenv.config();
-const fetch = require("node-fetch");
-
-
-
-const apiId = process.env.API_ID;
-const apiKey = process.env.API_KEY;
+const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
 
 app.use(express.static('dist'))
 
 console.log(__dirname)
-
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
@@ -29,44 +18,6 @@ app.listen(8080, function () {
     console.log('Example app listening on port 8080!')
 })
 
-
-app.post('/analyze', function(req, res){
-    let newData = req.body;
-    let newText = newData.text;
-    console.log(newText);
-    getNewsData("https://api.aylien.com/news/autocompletes?type=source_names&term="+newText)
-    .then(function(data){
-        let newEntry = {
-            autocompletes: data.autocompletes
-        };
-        console.log("this worked 1" + newEntry);
-        console.log(newEntry);
-        res.send(newEntry)
-    })
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
 })
-
-
-const getNewsData = async (url = '') =>{
-    const res = await fetch(url, {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-            'X-AYLIEN-NewsAPI-Application-ID': apiId,
-            'X-AYLIEN-NewsAPI-Application-Key': apiKey
-        }
-    });
-    
-    try{
-        const data = await res.json();
-        console.log("this worked 2" + data);
-        console.log(data);
-        let newEntry = {
-            autocompletes: data.autocompletes
-        }
-        console.log("this worked 3" + newEntry);
-        console.log(newEntry);
-        return data;
-    } catch(error){
-        console.log("error",error);
-    }
-}
